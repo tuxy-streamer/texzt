67add288796e63c9640bf87c727a554c730795f9
tuxy-streamer <subham020304@gmail.com>
2026-03-01 01:30:58 +0530

    chore: add cmake build files

---
+++ build/CMakeFiles/texzt.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/texzt.dir/link.d"
+  "CMakeFiles/texzt.dir/main.cpp.o"
+  "CMakeFiles/texzt.dir/main.cpp.o.d"
+  "texzt"
+  "texzt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/texzt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
