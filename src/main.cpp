443f435ba13a05c9671ccbba550d483413dc59ae
tuxy-streamer <subham020304@gmail.com>
2026-03-01 04:13:00 +0530

    refactor: add create_window function for making windows

--- src/main.cpp
+++ src/main.cpp
@@ -3,31 +3,32 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char *argv[]) {
-  glfwInit();
-  // HACK: Version hinting
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-  GLFWwindow *window = glfwCreateWindow(800, 800, "Texzt", NULL, NULL);
-
+int create_window(int width, int height, char *title) {
+  GLFWwindow *window = glfwCreateWindow(width, height, title, NULL, NULL);
   if (window == NULL) {
     cout << "Failed to create window." << endl;
     glfwTerminate();
-	return -1;
+    return -1;
   }
-
   glfwMakeContextCurrent(window);
-
   while (!glfwWindowShouldClose(window)) {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
-
   glfwDestroyWindow(window);
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+  glfwInit();
+  // HACK: Version hinting
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  char *title = (char *)("Texzt");
+  create_window(800, 800, title);
   glfwTerminate();
   return 0;
 }
